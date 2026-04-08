f3867a63901e702fa3ae4265d7f66d6fadc77d91
bayuseta566-bfr <bayuseta566@gmail.com>
2026-04-08 18:16:03 +0700

    secound upload

---
+++ script.js
@@ -0,0 +1,66 @@
+let pesanan = [];
+let total = 0;
+
+// TAMBAH MENU
+function tambah(nama, harga) {
+    pesanan.push({ nama, harga });
+    total += harga;
+    render();
+}
+
+// TAMPILKAN LIST
+function render() {
+    let list = document.getElementById("list");
+    list.innerHTML = "";
+
+    pesanan.forEach(p => {
+        let li = document.createElement("li");
+        li.textContent = `${p.nama} - Rp ${p.harga}`;
+        list.appendChild(li);
+    });
+
+    // tampilkan total
+    document.getElementById("total").innerText = total;
+}
+
+// HITUNG KEMBALIAN
+document.getElementById("uang").addEventListener("input", function() {
+    let uang = parseInt(this.value) || 0;
+    let kembali = uang - total;
+
+    document.getElementById("kembalian").value = kembali >= 0 ? kembali : "Uang kurang";
+});
+
+// SELESAI PESANAN
+function selesai() {
+    if (pesanan.length === 0) return alert("Belum ada pesanan!");
+
+    kirimData();
+
+    alert("Pesanan selesai!");
+
+    pesanan = [];
+    total = 0;
+
+    document.getElementById("uang").value = "";
+    document.getElementById("kembalian").value = "";
+    document.getElementById("total").innerText = 0;
+
+    render();
+}
+
+// KIRIM KE GOOGLE SHEETS
+async function kirimData() {
+
+    const url = "https://script.google.com/macros/s/AKfycbzocqRqr2ZAi9w31np5rw-n8k5g_LqOkjG8mQxr6kc_YJ9yVNsfBcskNbgOh0Pql1p_/exec";
+
+    let data = pesanan.map(p => ({
+        nama: p.nama,
+        harga: p.harga
+    }));
+console.log(data);
+    await fetch(url, {
+        method: "POST",
+        body: JSON.stringify(data)
+    });
+}

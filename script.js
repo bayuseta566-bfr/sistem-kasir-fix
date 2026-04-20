7527349641a802057f8ccc609604e7550e340e5a
bayuseta566-bfr <bayuseta566@gmail.com>
2026-04-20 19:43:43 +0700

    savee

--- script.js
+++ script.js
@@ -1,70 +1,76 @@
 let pesanan = [];
 let total = 0;
 
-function tambah(nama, harga) {
-    pesanan.push({ nama, harga });
+function tambah(nama,harga){
+    let item = pesanan.find(p=>p.nama===nama);
+
+    if(item){
+        item.qty++;
+        item.subtotal += harga;
+    }else{
+        pesanan.push({
+            nama:nama,
+            harga:harga,
+            qty:1,
+            subtotal:harga
+        });
+    }
+
     total += harga;
     render();
 }
 
-function render() {
+function render(){
     let list = document.getElementById("list");
     list.innerHTML = "";
 
-    pesanan.forEach(p => {
-        let li = document.createElement("li");
-        li.textContent = `${p.nama} - Rp ${p.harga}`;
-        list.prepend(li);
+    pesanan.forEach(p=>{
+        list.innerHTML += `
+        <div class="order-item">
+            <span>${p.nama} x${p.qty}</span>
+            <span>Rp ${p.subtotal}</span>
+        </div>
+        `;
     });
 
     document.getElementById("total").innerText = total;
 }
 
-document.getElementById("uang").addEventListener("input", function () {
-    let uang = parseInt(this.value) || 0;
-    let kembali = uang - total;
-
-    document.getElementById("kembalian").value =
-        kembali >= 0 ? kembali : "Uang kurang";
+document.getElementById("uang").addEventListener("input", function(){
+    let uang = parseInt(this.value)||0;
+    let kembali = uang-total;
+    document.getElementById("kembalian").value = kembali >=0 ? kembali : "Uang kurang";
 });
 
-aasync function kirimData(nama) {
+async function selesai(){
+
+    let namaPelanggan = document.getElementById("namaPelanggan").value;
+    if(!namaPelanggan) return alert("Isi nama pelanggan");
 
     const url = "https://script.google.com/macros/s/AKfycbzWLXTpYMp_YYx_uwLiMjVDUFtxwnMHHe7UCMSZh_2BoKsf6BlB2CoHih2_VFPhlx7U/exec";
 
-    let data = {
-        nama: nama,
-        pesanan: pesanan,
-        total: total
-    };
+    for(let item of pesanan){
+        await fetch(url,{
+            method:"POST",
+            mode:"no-cors",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                waktu:new Date().toLocaleString(),
+                pelanggan:namaPelanggan,
+                menu:item.nama,
+                qty:item.qty
+            })
+        });
+    }
 
-    await fetch(url, {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    });
-}
-
-function selesai() {
-
-    let nama = document.getElementById("nama").value;
-
-    if (!nama) return alert("Masukkan nama!");
-    if (pesanan.length === 0) return alert("Kosong!");
-
-    kirimData(nama);
-
-    alert("Berhasil!");
+    alert("Pesanan selesai");
 
     pesanan = [];
     total = 0;
-
-    document.getElementById("nama").value = "";
-    document.getElementById("uang").value = "";
-    document.getElementById("kembalian").value = "";
-
+    document.getElementById("namaPelanggan").value="";
+    document.getElementById("uang").value="";
+    document.getElementById("kembalian").value="";
     render();
 }
